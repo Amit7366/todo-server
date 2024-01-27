1614456e07f1672025a7d39e5f547d9a541dd0c8
Sabbir386 <sabbir386.sa@gmail.com>
2024-01-28 00:24:39 +0600

    total

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-const cors = require('cors');
+const cors = require('cors'); 
 
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb://localhost:27017`;
+const uri = `mongodb+srv://todo:todopass@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
